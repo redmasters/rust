c59aba251369052820709a2509b52e5862ee2924
Erasmo Jr. <erasmo.cjunior@gmail.com>
2026-05-31 00:14:06 -0300

    feat: first exercicio

--- exercicios/001.rs
+++ exercicios/001.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::io;
 
 fn main(){
@@ -18,14 +19,32 @@ fn main(){
     println!("Insira o primeiro numero:");
 
     let mut number_a = String::new();
-    let mut number_b = String::new();
 
     io::stdin()
         .read_line(&mut number_a)
-        .expect("Falha ao ler o numero")
+        .expect("Falha ao ler o numero");
+    let number_a: u32 = number_a.trim()
+        .parse()
+        .expect("Por favor insira o primeiro numero");
+
+    println!("Voce inseriu o numero {number_a}");
+
+    println!("Insira o segundo numero:");
+    let mut number_b = String::new();
+
+    io::stdin()
+        .read_line(&mut number_b)
+        .expect("Falha ao ler o numero");
 
 
-    let number_a: u32 = number_a.trim().parse().expect("Por favor insira um numero")
-    let number_b: u32 = number_b.trim().parse().expect("Por favor insira um numero")
+    let number_b: u32 = number_b.trim()
+        .parse()
+        .expect("Por favor insira o segundo numero");
+
+    match number_a.cmp(&number_b) {
+        Ordering::Less => println!("{number_a} eh menor que {number_b}"),
+        Ordering::Greater=> println!("{number_a} eh maior que {number_b}"),
+        Ordering::Equal=> println!("{number_a} eh igual a {number_b}")
+    }
 
 }
